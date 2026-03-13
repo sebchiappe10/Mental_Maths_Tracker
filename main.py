1224514223d4b3cf5058fa59403235e54e2547ca
sebchiappe10 <sebchiappe10@gmail.com>
2026-03-13 14:32:26 +0000

    Created main.py
    The script that a user would run to start the quiz.

---
+++ main.py
@@ -0,0 +1,16 @@
+from mental_maths_quiz import run_session
+
+def main():
+    print("Welcome to Mental Maths Tracker")
+    level = int(input("Enter level (1-5): "))
+    run_session(level)
+    return None
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+main()
+
